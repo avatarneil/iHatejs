55e33959e1922a53182ca7b3096a61f9e4c1cba8
Neil <avatarneil@gmail.com>
2017-05-20 19:16:55 -0400

    init commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var taskData = {tasks:[]};
+function onload() {
+  foo = document.getElementById('inputID');
+}
+function addTask(){
+  var task = { task: {info: foo.value, date: new Date()}};
+  taskData.tasks.push(task);
+  console.log('addTask ran!');
+}
